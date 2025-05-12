2bff0d895dd134f68f24e01ced9d7f0d03d23d17
Bryan Delianov <bryan.delianov@gmail.com>
2025-05-12 11:58:20 +0200

    Mise à jour du répertoire git

--- database/migrations/2025_05_12_091341_create_conversations_table.php
+++ database/migrations/2025_05_12_091341_create_conversations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('conversation', function (Blueprint $table) {
+        Schema::create('conversations', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
             $table->timestamp('date_created')->useCurrent();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('conversation');
+        Schema::dropIfExists('conversations');
     }
 };
