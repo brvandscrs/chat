8dd40539f83deaddc78d2cd92aa6c6ddc26889bd
Bryan Delianov <bryan.delianov@gmail.com>
2025-05-12 15:03:35 +0200

    Mise à jour du répertoire git

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index() : View
+    {
+        return view('test');
+        
+        // Récupère tous les messages dans la variable $message
+        // et les ordonne par date_heure croissante
+        $message = Message::query()
+        ->ord
+        ->get();
+        // return view('home');
+        return view('home', compact('messages'));
+    }
 }
