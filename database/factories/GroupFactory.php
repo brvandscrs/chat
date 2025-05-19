86e2b88a732db785f1d67d480a4af4c99cee1cdb
Bryan Descours <bryan.delianov@gmail.com>
2025-05-19 14:48:02 +0200

    Ajout de la table groups avec le model, factory et seeder.

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\groups>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class groupsFactory extends Factory
+class GroupFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/migrations/2025_05_19_101335_create_groups_table.php
+++ database/migrations/2025_05_19_101335_create_groups_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps('created_at')->useCurrent();
+            $table->timestamps('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
